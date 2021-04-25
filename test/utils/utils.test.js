ee98e5d6c54312d2e915b292774afa247da09cd6
CY Kalu <cy23@tutanota.com>
2021-04-25 02:47:28 +0100

    Refactor to eliminiate 'object-curly-newline' error

--- test/utils/utils.test.js
+++ test/utils/utils.test.js
@@ -1,22 +1,22 @@
-import { generateRandomStr, randomID, parser, setAttributes } from '../../src/js/utils/utils';
+import * as utils from '../../src/js/utils/utils';
 
 describe('parser', () => {
   it('returns a parsed html from strings', () => {
     const el = document.createElement('div');
-    const parsedHTML = parser('<h1>H1 tag</h1>');
+    const parsedHTML = utils.parser('<h1>H1 tag</h1>');
     expect(() => el.appendChild(parsedHTML)).not.toThrow(TypeError);
   });
 });
 
 describe('generateRandomStr', () => {
   it('generates string with length of passed argument', () => {
-    expect(generateRandomStr().length).toEqual(1);
+    expect(utils.generateRandomStr().length).toEqual(1);
   });
 });
 
 describe('randomID', () => {
   it('generates a random ID matching pattern', () => {
-    expect(randomID()).toMatch(/^[A-Z]{1}\d{8,10}$/);
+    expect(utils.randomID()).toMatch(/^[A-Z]{1}\d{8,10}$/);
   });
 });
 
@@ -24,7 +24,7 @@ describe('setAttributes', () => {
   it('sets given attributes to the passed element', () => {
     const el = document.createElement('a');
     const attributes = { id: 'testID', href: '#', 'data-temp': '4567' };
-    setAttributes(el, attributes);
+    utils.setAttributes(el, attributes);
 
     Object.keys(attributes).forEach((attr) => {
       expect(el.hasAttribute(attr)).toBe(true);
